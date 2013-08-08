1662297b0eae29bdbf25a2b0f219ad5071d7237f
lester_hu <jwhu1024@gmail.com>
2013-08-08 18:03:30 +0800

    first import

---
+++ Node-Beginner-Book/server.js
@@ -0,0 +1,28 @@
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+	function onRequest(request, response) {
+		/* Ignored favicon requested */
+		if (request.url === "/favicon.ico") {
+			response.writeHead(200, {
+				"Content-Type": "image/x-icon"
+			});
+			response.end();
+			return;
+		}
+
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+		route(handle, pathname, response, request);
+		//route(handle, pathname, response);
+		//response.writeHead(200, {"Content-Type": "text/plain"});
+		//var content = route(handle, pathname);
+		//response.write(content);
+		//response.end();
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
+}
+
+exports.start = start;
