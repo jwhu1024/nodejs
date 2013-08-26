a20446d97819be0e2b63093dad01f88794fbb106
lester_hu <jwhu1024@gmail.com>
2013-08-26 14:57:38 +0800

    login with express and passport

---
+++ Node-Adv-Topic/login_express_passport/app.js
@@ -0,0 +1,88 @@
+var flash           = require("connect-flash"),
+	express         = require("express"),
+	passport        = require("passport"),
+	util            = require("util"),
+	LocalStrategy   = require("passport-local").Strategy,
+	fs				= require("fs"),
+	auth			= require("./auth.js");
+
+passport.serializeUser(function(user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+	auth.findById(id, function(err, user) {
+		done(err, user);
+	});
+});
+
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+		process.nextTick(function() {
+			auth.findByUsername(username, function(err, user) {
+				if (err) {
+					return done(err);
+				}
+				
+				if (!user) {
+					return done(null, false, {
+						message: "Unknown user " + username
+					});
+				}
+
+				if (user.password != password) {
+					return done(null, false, {
+						message: "Invalid password"
+					});
+				}
+				return done(null, user);
+			});
+		});
+	}
+));
+
+var app = express();
+
+// configure Express
+app.configure(function() {
+	app.set("views", __dirname + "/views");
+	app.set("view engine", "ejs");
+	app.use(express.logger("tiny"));
+	app.use(express.cookieParser());
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.session({
+		secret: "IdczacmUfNmqb7RQN8WFzO7iYC7ujXSyIPyumBscDCgRdtc0b4"
+	}));
+	app.use(flash());
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(app.router);
+	app.use(express.static(__dirname + "/../../public"));
+});
+
+app.get("/", auth.ensureAuthenticated, function(req, res) {
+	res.render("index", {
+		user: req.user
+	});
+});
+
+app.get("/login", function(req, res) {
+	res.render("login", { user: req.user, message: req.flash("error") });
+});
+
+app.post("/login",
+	passport.authenticate("local", {
+		failureRedirect: "/login",
+		failureFlash: true
+	}),
+	function(req, res) {
+		res.end("/");
+	});
+
+app.get("/logout", function(req, res) {
+	req.logout();
+	res.redirect("/");
+});
+
+app.listen(3000);
---
+++ Node-Adv-Topic/login_express_passport/auth.js
@@ -0,0 +1,29 @@
+users = require("./public/users.json");
+
+var MAX_USER_CNT = 2;
+
+exports.findById = function (id, fn) {
+	if (users[id]) {
+		fn(null, users[id]);
+	} else {
+		fn(new Error('User ' + id + ' does not exist'));
+	}
+};
+
+exports.findByUsername = function (username, fn) {
+	for (var i = 1; i <= MAX_USER_CNT; i++) {
+		if (users[i].username === username) {
+			return fn(null, users[i]);
+		}
+	}
+	return fn(null, null);
+};
+
+exports.ensureAuthenticated = function (req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/login');
+};
+
+exports.users = users;
