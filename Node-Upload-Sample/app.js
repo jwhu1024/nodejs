052e3f0a178a17700e5128bf01f76495a07904ea
lester_hu <jwhu1024@gmail.com>
2013-09-04 19:17:47 +0800

    update - 20130904

--- Node-Upload-Sample/app.js
+++ Node-Upload-Sample/app.js
@@ -1,6 +1,7 @@
-var express = require("express"),
-    upload  = require("./upload.js"),
-    app     = express();
+var express      = require("express"),
+    upload       = require("./upload.js"),
+    uploadFolder = __dirname + upload.setting.uploadDir,
+    app          = express();
 
 // configure Express
 app.configure(function() {
@@ -8,9 +9,11 @@ app.configure(function() {
     app.set("view engine", "ejs");
     app.use(express.logger("tiny"));
     app.use(express.cookieParser());
+    //app.use(express.limit("4mb"));
     app.use(express.bodyParser({
         keepExtensions: true,
-        defer: true
+        defer: true,
+        uploadDir: uploadFolder
     }));
     app.use(app.router);
     app.use(express.static(__dirname + upload.setting.uploadDir));
@@ -24,19 +27,19 @@ app.get("/", function(req, res) {
 
 // download
 app.get("/attachment/:fileName", function(req, res, next) {
-    var downloadFile = __dirname + upload.setting.uploadDir + req.params.fileName;
+    var downloadFile = uploadFolder + req.params.fileName;
     require("fs").exists(downloadFile, function (exists) {
         if (exists) {
             res.download(downloadFile);
         } else {
             res.end("File Not Found");
         }
-    });    
+    });
 });
 
 // upload
 app.post("/upload_form", function(req, res) {
-    upload.handleFileUpload(req, res);
+    upload.handleFileUpload(req, res, uploadFolder);
 });
 
 app.listen(3000);
--- Node-Upload-Sample/upload.js
+++ Node-Upload-Sample/upload.js
@@ -4,18 +4,284 @@ var fs            = require("fs"),
 
 exports.setting = uploadSetting;
 
+exports.handleFileUpload = function(req, res, uploadDir) {
+    var uploadObj,
+        oldPath,
+        newPath,
+        prevPercent=0,
+    procFileEvent = function(name, file) {
+        util.log("Handle formidable file event");
+        uploadObj = file;
+        oldPath   = uploadObj.path;
+        newPath   = uploadDir + uploadObj.name;
+
+        if (uploadSetting.limitEnable && (file.size > uploadSetting.limit)) {
+            console.log(file.size + "size######################");
+            // remove temp file
+            _unlinkFile(oldPath);
+
+            // unregister our listener
+            _handleEvent(req, regEvent, false);
+
+            // display limit
+            util.log(util.format("%d larger than %d", file.size, uploadSetting.limit));
+
+            // end of this request
+            res.end("Exceeds the upper limit");
+        }
+    },
+    procProgressEvent = function(bytesReceived, bytesExpected) {
+        // display progress
+        var curPercent = Math.round((bytesReceived / bytesExpected) * 100);
+
+        if (prevPercent !== curPercent) {
+            util.log(curPercent + "% uploaded");
+            prevPercent = curPercent;
+        }
+    },
+    procEndEvent = function() {
+        util.log("Handle formidable end event");
+        // do something when form transfer complete
+        fs.rename(oldPath, newPath, function () {
+            util.log("upload succeed");
+            res.render("result", {
+                uploadInfo: uploadObj
+            });
+        });
+    },
+    procAbortEvent = function() {
+        util.log("Handle formidable abort event");
+        _unlinkFile(oldPath);
+        // unregister our listener
+        _handleEvent(req, regEvent, false);
+    },
+    procErrorEvent = function(err) {
+        // just return error message
+        res.end("Unknown Error");
+    },
+    // Event & Callback
+    regEvent = {
+        file     : procFileEvent,
+        progress : procProgressEvent,
+        end      : procEndEvent,
+        aborted  : procAbortEvent,
+        error    : procErrorEvent
+    };
+
+    // register event listener
+    _handleEvent(req, regEvent, true);
+};
+
+/**
+ * [handleEvent description]
+ * @param action [true: register, false: unregister]
+ */
+function _handleEvent(req, regEvent, reg) {
+    // skip progress event if disabled
+    if (uploadSetting.progress === false) {
+        regEvent.progress=null;
+    }
+
+    for (var localEvent in regEvent) {
+        if (regEvent[localEvent]) {
+            if (reg) {
+                // Register event
+                req.form.on(localEvent, regEvent[localEvent]);    
+            } else {
+                // Unregister event
+                req.form.removeListener(localEvent, regEvent[localEvent]);
+            }
+        }
+    }
+}
+
+function _unlinkFile (_path) {
+    fs.exists(_path, function(exists) {
+        if (exists) {
+            fs.unlink(_path, function (err) {
+                util.log("successfully deleted file");
+            });    
+        }        
+    });
+}
+
+/*
+exports.handleFileUpload = function(req, res) {
+    var prevPercent = 0,
+        targetFile,
+        outputStream,
+    procProgress = function(bytesReceived, bytesExpected) {
+        // abort this request if data size exceeds the limit size
+        if (uploadSetting.limitEnable && bytesExpected > uploadSetting.limit) {
+            // unregister our listener
+            _handleEvent(req, regEvent, false);
+
+            // display limit
+            util.log(util.format("%d larger than %d", bytesExpected, uploadSetting.limit));
+
+            // end of this request
+            res.end("Exceeds the upper limit");
+        } else {
+            // display progress if enabled
+            if (uploadSetting.progress) {
+                var curPercent = Math.round((bytesReceived / bytesExpected) * 100);
+
+                if (prevPercent !== curPercent) {
+                    util.log(curPercent + "% uploaded");
+                    prevPercent = curPercent;
+                }
+            }
+        }
+    },
+    procEndEvent = function() {
+        util.log("end event trigger");
+        // do something when form transfer complete
+    },
+    procErrorEvent = function(err) {
+        // just print error message
+        res.end("Unknown Error");
+    },
+    procAbortEvent = function () {
+        if (targetFile) {
+            outputStream.end();
+            _unlinkFile(targetFile);
+        }
+    },
+    // Event & Callback
+    regEvent = {
+        progress: procProgress,
+        end     : procEndEvent,
+        error   : procErrorEvent,
+        aborted : procAbortEvent
+    };
+    
+    // register event listener
+    _handleEvent(req, regEvent, true);
+
+    // overwrite this method for directly accessing the multipart stream
+    req.form.onPart = function(part){
+        if (!part.filename) {
+            // let formidable handle all non-file parts
+            req.form.handlePart(part);
+            _handleEvent(req, regEvent, false);
+            res.end("File is not selected!");
+            return;
+        } else {
+            targetFile = uploadDir + part.filename;
+
+            // remove previous temp file
+            _unlinkFile(targetFile);
+            
+            // create writable stream
+            outputStream = fs.createWriteStream(targetFile, {
+                flags: "w+",
+                autoclose: true,
+                bufferSize: 209715200
+            });
+
+            // listen on data event
+            part.on("data", function(raw) {
+                outputStream.write(raw);
+            });
+
+            // listen on end event
+            part.on("end", function() {
+                res.render("result", {
+                    uploadInfo: part
+                });
+                outputStream.end();
+            });
+
+            // listen on error event
+            part.on("error", function(err) {
+                util.log("###################");
+            });
+        }
+    };
+
+    // overwrite this method for directly accessing the multipart stream
+    req.form.onPart = function(part){
+        if (!part.filename) {
+            // let formidable handle all non-file parts
+            req.form.handlePart(part);
+            _handleEvent(req, regEvent, false);
+            res.end("File is not selected!");
+            return;
+        } else {
+            targetFile = uploadDir + part.filename;
+
+            // remove previous temp file
+            _unlinkFile(targetFile);
+            
+            // create writable stream
+            outputStream = fs.createWriteStream(targetFile, {
+                flags: "w+",
+                autoclose: true
+            });
+
+            inputStream = fs.createReadStream(file.path, {
+                flags: "r+",
+                autoclose: true
+            });
+
+            // listen on data event
+            part.on("data", function(raw) {
+                if (outputStream.write(raw) === false) {
+                    part.pause();
+                }
+            });
+
+            // listen on end event
+            part.on("end", function() {
+                res.render("result", {
+                    uploadInfo: part
+                });
+                outputStream.end();
+            });
+
+            // listen on error event
+            part.on("error", function(err) {
+                util.log("###################");
+            });
+
+            // listen on drain event
+            outputStream.on("drain", function() {
+                part.resume();
+            });
+        }
+    };
+    
+};
+
 exports.handleFileUpload = function(req, res) {
     var prevPercent = 0,
     procFileEvent = function(name, file) {
+        console.log(file);
         if (file.name) {
             var newPath = __dirname + uploadSetting.uploadDir + file.name,
             input = fs.createReadStream(file.path, {
                 flags: "r+",
-                autoclose: true
+                autoclose: true,
+                bufferSize: 2097152
             }),
             output = fs.createWriteStream(newPath, {
                 flags: "w+",
                 autoclose: true
+            });
+
+            input.pipe(output);
+
+            output.on("close", function () {
+                console.log("Close#########################");
+                // remove temp file
+                fs.unlink(req.files.upload.path, null);
+                
+                // return result page
+                res.render("result", {
+                    uploadInfo: file
+                });
+
+                output.end();
             });
 
             input.on("data", function(chunk) {
@@ -25,10 +291,10 @@ exports.handleFileUpload = function(req, res) {
             });
 
             input.on("close", function() {
-                /* remove temp file */
+                // remove temp file
                 fs.unlink(req.files.upload.path, null);
                 
-                /* return result page */
+                // return result page
                 res.render("result", {
                     uploadInfo: file
                 });
@@ -45,24 +311,25 @@ exports.handleFileUpload = function(req, res) {
             output.on("error", function(err) {
                 util.log("output error event");
             });
+
         } else {
             handleEvent(req, regEvent, false);
             res.end("File is not selected!");
-        }        
+        }
     },
     procProgress = function(bytesReceived, bytesExpected) {
-        /* abort this request if data size exceeds the limit size */
+        // abort this request if data size exceeds the limit size
         if (uploadSetting.limitEnable && bytesExpected > uploadSetting.limit) {
-            /* unregister our listener */
+            // unregister our listener
             handleEvent(req, regEvent, false);
 
-            /* display limit */
+            // display limit
             util.log(util.format("%d larger than %d", bytesExpected, uploadSetting.limit));
 
-            /* end of this request */
+            // end of this request
             res.end("Exceeds the upper limit");
         } else {
-            /* display progress if enabled */
+            // display progress if enabled
             if (uploadSetting.progress) {
                 var curPercent = Math.round((bytesReceived / bytesExpected) * 100);
 
@@ -77,10 +344,10 @@ exports.handleFileUpload = function(req, res) {
         // do something when form transfer complete
     },
     procErrorEvent = function(err) {
-        /* just print error message */
+        // just print error message
         res.end("Unknown Error");
     },    
-    /* Event & Callback */
+    // Event & Callback
     regEvent = {
         progress : procProgress,
         end      : procEndEvent,
@@ -88,22 +355,8 @@ exports.handleFileUpload = function(req, res) {
         file     : procFileEvent
     };
     
-    /* register event listener */
+    // register event listener
     handleEvent(req, regEvent, true);
 };
+*/
 
-/**
- * [handleEvent description]
- * @param action [true: register, false: unregister]
- */
-function handleEvent(req, regEvent, action) {
-    for (var localEvent in regEvent) {
-        if (action) {
-            /* Register event */
-            req.form.on(localEvent, regEvent[localEvent]);
-        } else {
-            /* Unregister event */
-            req.form.removeListener(localEvent, regEvent[localEvent]);
-        }
-    }
-}
