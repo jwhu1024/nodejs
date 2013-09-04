43fca87ee6cfd9e870c579271c080ae1af87d2c2
lester_hu <jwhu1024@gmail.com>
2013-09-05 01:32:59 +0800

    add http+multipart for test memory usage

--- Node-Upload-Sample/app.js
+++ Node-Upload-Sample/app.js
@@ -43,3 +43,68 @@ app.post("/upload_form", function(req, res) {
 });
 
 app.listen(3000);
+
+
+/*
+var http = require('http'),
+    util = require('util'),
+    multiparty = require('multiparty'),
+    fs = require('fs'),
+    PORT = process.env.PORT || 3000;
+
+var server = http.createServer(function(req, res) {
+    if (req.url === '/') {
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        });
+        res.end(
+            '<form action="/upload" enctype="multipart/form-data" method="post">' +
+            '<input type="text" name="title"><br>' +
+            '<input type="file" name="upload" multiple="multiple"><br>' +
+            '<input type="submit" value="Upload">' +
+            '</form>'
+        );
+    } else if (req.url === '/upload') {
+        var form = new multiparty.Form({uploadDir: __dirname + "\\upload_dir\\"});
+
+        form.parse(req, function(err, fields, files) {
+            if (err) {
+                res.writeHead(400, {
+                    'content-type': 'text/plain'
+                });
+                res.end("invalid request: " + err.message);
+                return;
+            }
+            res.writeHead(200, {
+                'content-type': 'text/plain'
+            });
+
+            // test area
+            //var oldPath = files.upload.path,
+            //    newPath = __dirname + "\\upload_dir\\" + files.upload.originalFilename;
+            //fs.rename(oldPath, newPath, function () {
+            //    console.log("upload succeed");
+            //});
+            // test area
+
+            res.write('received fields:\n\n ' + util.inspect(fields));
+            res.write('\n\n');
+            res.end('received files:\n\n ' + util.inspect(files));
+        });
+
+        form.on('progress', function(bytesReceived, bytesExpected) {
+            util.log(Math.round((bytesReceived / bytesExpected) * 100));
+        })
+    } else {
+        res.writeHead(404, {
+            'content-type': 'text/plain'
+        });
+        res.end('404');
+    }
+
+
+});
+server.listen(PORT, function() {
+    console.info('listening on http://0.0.0.0:' + PORT + '/');
+});
+*/
